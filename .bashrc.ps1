5edcbb70744ec0ea5847fd768bcc8773e7dd5119
Jameson Williams <jhwill@amazon.com>
2017-03-13 11:56:42 -0700

    fix the color script geez

--- .bashrc.ps1
+++ .bashrc.ps1
@@ -1,30 +1,40 @@
+[ -z "$term_reset" ] &&
+    readonly term_reset="\[$(tput sgr0)\]"
 
-reset="\[$(tput sgr0)\]"
-bold="\[$(tput bold)\]"
-gray="245"
-
-directory='\w'
-host='\h'
-user='\u'
-
-function get_color_for_host() {
-    unique_id=$(hostname -s | cksum | cut -f1 -d" ")
-    host_color=$(($(($unique_id % 130)) + 50))
+get_color_for_host() {
+    local readonly unique_id=$(hostname -s | cksum | cut -f1 -d" ")
+    local readonly host_color=$(($(($unique_id % 130)) + 40))
 
     echo "$host_color"
 }
 
-function colorize() {
-    target="$1"
-    color="${2-$(get_color_for_host)}"
+colorize() {
+    # Note: you want `tput setaf` to work so bad, but it just doesn't.
+    local readonly color_format="\[\033[38;5;%dm\]"
 
-    echo -n "\[$(tput setaf $color)\]${target}${reset}"
+    local readonly default_color="$(get_color_for_host)"
+    local readonly target="$1"
+    local readonly color="${2-$default_color}"
+
+    printf "${color_format}${target}${term_reset}" "$color"
 }
 
-PS1="${user}@"
-PS1="${PS1}${bold}$(colorize ${host}):"
-PS1="${PS1}$(colorize $directory $gray)$ "
-PS1="${PS1}${reset}"
+build_ps1() {
+    local readonly bold="\[$(tput bold)\]"
+    local readonly gray="245"
 
-export PS1
+    local readonly directory='\w'
+    local readonly host='\h'
+    local readonly user='\u'
 
+    local prompt=''
+
+    prompt="${user}@"
+    prompt="${prompt}${bold}$(colorize ${host}):"
+    prompt="${prompt}$(colorize $directory $gray)$ "
+    prompt="${prompt}${term_reset}"
+    
+    echo -n "${prompt}"
+}
+
+export PS1="$(build_ps1)"
